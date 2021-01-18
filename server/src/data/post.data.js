a468d1cca7db9ea050a9c1ff556062f8b3c5e8db
DESKTOP-M1GA9T9\Admin <hovanvu26699@gmail.com>
2021-01-18 16:16:28 +0700

    final

---
+++ server/src/data/post.data.js
@@ -0,0 +1,10 @@
+module.exports.posts = [
+    {
+        name: 'Minh Quý',
+        content: 'Hôm nay trời đẹp quá mọi người ơi'
+    },
+    {
+        name: 'Minh Quý',
+        content: 'Hôm nay tự nhiên buồn'
+    }
+]
